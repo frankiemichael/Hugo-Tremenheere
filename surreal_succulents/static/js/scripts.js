dd9cf6f3f6df5aa755741728377b6b6c6f065b57
frankiemichael <frankiemichael96@gmail.com>
2021-01-13 17:00:47 +0000

    Add files via upload

--- surreal_succulents/static/js/scripts.js
+++ surreal_succulents/static/js/scripts.js
@@ -3,13 +3,11 @@
 function openNav() {
   document.getElementById("mySidebar").style.width = "35%";
   document.getElementById('bodyId').classList.add("opaquehidden");
-  document.getElementById('headerDiv').style.width = "auto";
   document.getElementById('opaque').style.pointerEvents= "none";
 }
 function closeNav() {
   document.getElementById("mySidebar").style.width = "0";
 document.getElementById('bodyId').classList.remove("opaquehidden");
-  document.getElementById('headerDiv').style.width = "auto";
   document.getElementById('opaque').style.pointerEvents= "all";
   document.getElementById('bodyId').onclick = closeNav();
 
