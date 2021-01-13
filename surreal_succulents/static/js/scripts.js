bc0bb7e80ac267bce2bc891f90db2dec061b2768
frankiemichael <frankiemichael96@gmail.com>
2021-01-13 10:39:52 +0000

    Add files via upload

--- surreal_succulents/static/js/scripts.js
+++ surreal_succulents/static/js/scripts.js
@@ -1,25 +1,14 @@
 
-function openNav() {
-  document.getElementById("mySidepanel").style.width = "250px";
-}
-
-/* Set the width of the sidebar to 0 (hide it) */
-function closeNav() {
-  document.getElementById("mySidepanel").style.width = "0";
-}
 
 function openNav() {
-  document.getElementById('opaque').style.backgroundColor = "black";
-  document.getElementById("mySidebar").style.width = "250px";
-  document.getElementById('opaque').style.classList.add("opaquehidden");
+  document.getElementById("mySidebar").style.width = "35%";
+  document.getElementById('bodyId').classList.add("opaquehidden");
   document.getElementById('headerDiv').style.width = "auto";
   document.getElementById('opaque').style.pointerEvents= "none";
-
 }
 function closeNav() {
   document.getElementById("mySidebar").style.width = "0";
-  document.getElementById('opaque').style.opacity = "1";
-  document.getElementById('opaque').style.backgroundColor = "white";
+document.getElementById('bodyId').classList.remove("opaquehidden");
   document.getElementById('headerDiv').style.width = "auto";
   document.getElementById('opaque').style.pointerEvents= "all";
   document.getElementById('bodyId').onclick = closeNav();
