6e7bd4c6f673f199085deeae423dfd59a24cf692
frankiemichael <frankiemichael96@gmail.com>
2021-01-29 16:22:11 +0000

    Add files via upload

--- surreal_succulents/static/js/jquery.js
+++ surreal_succulents/static/js/jquery.js
@@ -2,8 +2,12 @@ $( document ).ready(function() {
 $("#wrapper").on("click", "button", function() {
   var offset = $('#offsetinput').val()
   var limit = $('#limitinput').val()
-  var host = 'https://app.snipcart.com/api/orders' + "?offset=" + offset + "&limit=" + limit
+  var status = $('#statusinput').val()
+  var invoicenumber = $('#invoicenumberinput').val()
+  var name = $('#nameinput').val()
+  var host = 'https://app.snipcart.com/api/orders' + "?offset=" + offset + "&limit=" + limit + "&status=" + status + "&invoiceNumber=" + invoicenumber + "&placedBy=" + name
   let secret = 'ST_MjZlOGQ4YjUtMzQwNC00OGM5LWI0MWYtOTE0YzliZDg2ZjFlNjM3NDY5MTIzMTA5ODA2NzAy'
+
     $.ajax({
         type: "GET",
         url: host,
@@ -15,7 +19,14 @@ $("#wrapper").on("click", "button", function() {
         dataType: 'json',
         success: function(result){
             console.log(result);
-            alert(host)
+            var apilink = result.items[0].billingAddress.city;
+            $('#apidata').append(apilink);
+            alert(host);
+            $.each(result, function(items) {
+
+              console.log(items.billingAddress)
+
+            })
         },
         error: function(error){
           console.log(error);
