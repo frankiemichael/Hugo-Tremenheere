5d4b30ec2c86b9be264772ac0f9b692d385190dd
frankiemichael <frankiemichael96@gmail.com>
2021-02-03 16:42:09 +0000

    Add files via upload

--- surreal_succulents/static/js/jquery.js
+++ surreal_succulents/static/js/jquery.js
@@ -2,13 +2,13 @@ $( document ).ready(function() {
 $("#wrapper").on("click", "button", function() {
   $('#apidata').empty();
   var offset = $('#offsetinput').val()
+  var executeButton = $('#executeButton').text("Execute");
   var limit = $('#limitinput').val()
   var status = $('#statusinput').val()
   var invoicenumber = $('#invoicenumberinput').val()
   var name = $('#nameinput').val()
   var host = 'https://app.snipcart.com/api/orders' + "?offset=" + offset + "&limit=" + limit + "&status=" + status + "&placedBy=" + name
-  let secret = 'ST_MjZlOGQ4YjUtMzQwNC00OGM5LWI0MWYtOTE0YzliZDg2ZjFlNjM3NDY5MTIzMTA5ODA2NzAy'
-
+  let secret = 'ST_MjZlOGQ4YjUtMzQwNC00OGM5LWI0MWYtOTE0YzliZDg2ZjFlNjM3NDY5MTIzMTA5ODA2NzAy' //hide this!!!!!!
     $.ajax({
         type: "GET",
         url: host + "&invoiceNumber=" + invoicenumber,
@@ -19,12 +19,10 @@ $("#wrapper").on("click", "button", function() {
         contentType: "application/json",
         dataType: 'json',
         success: function(result){
-            console.log(result);
             var obj = result.items;
             $.each(obj, function(i, val){
               var final = obj[i];
               var shipaddress = final.shippingAddress;
-              console.log(final);
               $('#apidata').append("<div id='invoicenumber'><button type='button' id='buttonclick' name='button2'><span id='buttonspan'>" + JSON.stringify(final.invoiceNumber) + "</span></button></div>" )
               $('#apidata').append("<div id='apiname'> <h4>" + JSON.stringify(shipaddress.fullName) + "</h4> </div>" )
               $('#apidata').append("<div id='apidate'> <h4>" + JSON.stringify(final.creationDate) + "</h4> </div>" )
@@ -46,7 +44,18 @@ $("#wrapper").on("click", "button", function() {
                     contentType: "application/json",
                     dataType: 'json',
                     success: function(result){
-                      console.log(result)
+                      var string = result.items[0]
+                      console.log(string)
+                      $('#apidata').empty();
+                      executeButton = $('#executeButton').text("Back");
+                      $('#apidata').append("<div id='apiname'> <h4>" + JSON.stringify(string.billingAddress.fullName) + "</h4> </div>" )
+                      $('#apidata').append("<div id='apidate'> <h4>" + JSON.stringify(string.creationDate) + "</h4> </div>" )
+                      $('#apidata').append("<div id='apistatus'> <h4>" + JSON.stringify(string.status) + "</h4> </div>" )
+                      $('#apidata').append("<div id='apishipping'><p>" + JSON.stringify(string.billingAddress.fullName) + "<br>" + JSON.stringify(string.billingAddress.address1) + "<br>" + JSON.stringify(string.billingAddress.address2) + "<br>" + JSON.stringify(string.billingAddress.city) + "<br>" + JSON.stringify(string.billingAddress.postalCode) + "<br>" + JSON.stringify(string.billingAddress.country) + "</p>")
+
+                      var quotes = document.getElementById("apidata").innerHTML;
+                      var removequotes = quotes.replace(/"/g, '');
+                      document.getElementById("apidata").innerHTML = removequotes;
                     }
 
                   });
@@ -54,7 +63,6 @@ $("#wrapper").on("click", "button", function() {
 
 
           });
-            alert(host);
           },
         error: function(error){
           console.log(error);
