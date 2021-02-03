9730711453b42f45f97f62eb8df52eee281b8a2c
frankiemichael <frankiemichael96@gmail.com>
2021-02-03 15:38:08 +0000

    Add files via upload

--- surreal_succulents/static/js/jquery.js
+++ surreal_succulents/static/js/jquery.js
@@ -6,12 +6,12 @@ $("#wrapper").on("click", "button", function() {
   var status = $('#statusinput').val()
   var invoicenumber = $('#invoicenumberinput').val()
   var name = $('#nameinput').val()
-  var host = 'https://app.snipcart.com/api/orders' + "?offset=" + offset + "&limit=" + limit + "&status=" + status + "&invoiceNumber=" + invoicenumber + "&placedBy=" + name
+  var host = 'https://app.snipcart.com/api/orders' + "?offset=" + offset + "&limit=" + limit + "&status=" + status + "&placedBy=" + name
   let secret = 'ST_MjZlOGQ4YjUtMzQwNC00OGM5LWI0MWYtOTE0YzliZDg2ZjFlNjM3NDY5MTIzMTA5ODA2NzAy'
 
     $.ajax({
         type: "GET",
-        url: host,
+        url: host + "&invoiceNumber=" + invoicenumber,
         headers: {
           'Authorization': `Basic ${btoa(secret)}`,
           'Accept': 'application/json'
@@ -25,7 +25,7 @@ $("#wrapper").on("click", "button", function() {
               var final = obj[i];
               var shipaddress = final.shippingAddress;
               console.log(final);
-              $('#apidata').append("<div id='invoicenumber'> <button type='button' name='button2'>" + JSON.stringify(final.invoiceNumber) + "</button> </div>" )
+              $('#apidata').append("<div id='invoicenumber'> <button type='button' id='buttonclick' name='button2'><span id='buttonspan'>" + JSON.stringify(final.invoiceNumber) + "</span></button></div>" )
               $('#apidata').append("<div id='apiname'> <h4>" + JSON.stringify(shipaddress.fullName) + "</h4> </div>" )
               $('#apidata').append("<div id='apidate'> <h4>" + JSON.stringify(final.creationDate) + "</h4> </div>" )
               $('#apidata').append("<div id='apistatus'> <h4>" + JSON.stringify(final.status) + "</h4> </div>" )
@@ -33,6 +33,24 @@ $("#wrapper").on("click", "button", function() {
               var quotes = document.getElementById("apidata").innerHTML;
               var removequotes = quotes.replace(/"/g, '');
               document.getElementById("apidata").innerHTML = removequotes;
+          }),
+              $("#apidata").on("click", "button", function() {
+                  var openinvoicenumber = $(this).parent().text();
+                  $.ajax({
+                    type: "GET",
+                    url: host + "&invoiceNumber=" + openinvoicenumber,
+                    headers: {
+                      'Authorization': `Basic ${btoa(secret)}`,
+                      'Accept': 'application/json'
+                    },
+                    contentType: "application/json",
+                    dataType: 'json',
+                    success: function(result){
+                      console.log(result)
+                    }
+
+                  });
+
 
 
           });
@@ -44,7 +62,5 @@ $("#wrapper").on("click", "button", function() {
     })
 
   })
-  $("#apidata").on("click", "button", function() {
-      alert("Hello")
-  });
+
 });
