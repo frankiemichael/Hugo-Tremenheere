2e98c0361f151749b57a1e6c8ff469c39fc7e03e
frankiemichael <frankiemichael96@gmail.com>
2021-02-10 15:28:02 +0000

    Add files via upload

--- surreal_succulents/static/js/jquery.js
+++ surreal_succulents/static/js/jquery.js
@@ -75,7 +75,12 @@ $("#executeButton").click (function(e) {
                         $('#wrapper').append("<div id='apiorderinfotop'><h4>Placed On:</h4>" + date  + "<br><br><h4>Order Status:</h4><select class='form-control' name='Status' id='statusinput' disabled><option value='" + string.status + "' selected='true' disabled='disabled'>" + string.status + "</option><option value ='InProgress'>In progress</option><option value ='Processed'>Processed</option><option value ='Disputed'>Disputed</option><option value ='Delivered'>Delivered</option><option value ='Shipped'>Shipped</option><option value ='Pending'>Pending</option><option value ='Cancelled'>Cancelled</option></select></div>")
                       $('#wrapper').append("<div id='apiorderinfotop'><h4>Billing Address:</h4><textarea id='billingfullname' readonly>" + JSON.stringify(string.billingAddress.fullName) + "</textarea><br><textarea id='billingaddress1' readonly>" + JSON.stringify(string.billingAddress.address1) + "</textarea><textarea id='billingaddress2' readonly>" + JSON.stringify(string.billingAddress.address2) + "</textarea><br><textarea id='billingaddresscity' readonly>" + JSON.stringify(string.billingAddress.city) + "</textarea><br><textarea id='billingaddresspostcode' readonly>" + JSON.stringify(string.billingAddress.postalCode) + "</textarea><br><textarea id='billingaddresscountry' readonly>" + JSON.stringify(string.billingAddress.country) + "</textarea></p>")
                       $('#wrapper').append("<div id='apiorderinfotop'><h4>Shipping Address:</h4><textarea id='shippingfullname' readonly>" + JSON.stringify(string.shippingAddress.fullName) + "</textarea><br><textarea id='shippingaddress1' readonly>" + JSON.stringify(string.shippingAddress.address1) + "</textarea><textarea id='shippingaddress2' readonly>" + JSON.stringify(string.shippingAddress.address2) + "</textarea><br><textarea id='shippingaddresscity' readonly>" + JSON.stringify(string.shippingAddress.city) + "</textarea><br><textarea id='shippingaddresspostcode' readonly>" + JSON.stringify(string.shippingAddress.postalCode) + "</textarea><br><textarea id='shippingaddresscountry' readonly>" + JSON.stringify(string.shippingAddress.country) + "</textarea></p>")
-                      $('#wrapper').append("<div id='apiorderinfotop'><h4>Customer Details</h4><img height='50px' width='50px' src=" + JSON.stringify(string.user.gravatarUrl) + "><br><div id='apicustomerdetails'>Phone: <textarea id='customerphone' readonly>" + JSON.stringify(string.customFields[0].value) + "</textarea></div><div id='apicustomerdetails'>Email: <textarea id='customeremail' readonly>" + JSON.stringify(string.user.email) + "</textarea></div><div id='apicustomerdetails'>User ID: " + JSON.stringify(string.user.id) + "</div><div id='apicustomerdetails'>Previous Orders: " + JSON.stringify(string.user.statistics.ordersCount) + "</div>");
+                      $('#wrapper').append("<div id='apiorderinfotop'><h4>Customer Details</h4><img height='50px' width='50px' src=" + JSON.stringify(string.user.gravatarUrl) + "><br><div id='apicustomerdetails'>Email: <textarea id='customeremail' readonly>" + JSON.stringify(string.user.email) + "</textarea></div><div id='apicustomerdetails'>User ID: " + JSON.stringify(string.user.id) + "</div><div id='apicustomerdetails'>Previous Orders: " + JSON.stringify(string.user.statistics.ordersCount) + "</div>");
+                      $('#wrapper').append("<div id='apiorderinfotop'><div id='apicustomerdetails'><h4>Delivery Note</h4><textarea id='deliverynote' readonly>" + JSON.stringify(string.customFields[0].value) + "</textarea></div>");
+                      $('#wrapper').append("<div id='apiorderinfotop'></div>");
+                      $('#wrapper').append("<div id='apiorderinfotop'></div>");
+                      $('#wrapper').append("<div id='apiorderinfotop'></div>");
+
 
                       $('#wrapper').append("<div id='soldproducts'><h3>Products</h3><br>")
                       $('#soldproducts').append("<div id='solditems'><div><h4>SKU</h4></div><div><h4>Name</h4></div><div><h4>Quantity</h4></div><div><h4>Individual Price</h4></div><div><h4>Total</h4></div></div>")
@@ -83,7 +88,7 @@ $("#executeButton").click (function(e) {
                       var product = string.items
                       $.each(product, function(i, val){
                       var productlist = product[i];
-                      $('#soldproducts').append("<div id='solditems'><div>" + JSON.stringify(productlist.customFields[0].value) + "</div><div>" + JSON.stringify(productlist.name) + "</div><div>" + JSON.stringify(productlist.quantity) + "</div><div>£" + JSON.stringify(productlist.price) + "</div><div>£" + JSON.stringify(productlist.totalPrice) + "</div></div>")
+                      $('#soldproducts').append("<div id='solditems'><div>" + JSON.stringify(productlist.customFields[0].value) + "</div><div>" + JSON.stringify(productlist.name) + "</div><div class='qtydiv'><button type='button' id='plus' class='qtybutton' style='display:none;'>+</button><input type='text' name='qty' id='qty' value='" + JSON.stringify(productlist.quantity) + "'readonly></input><button type='button' id='minus' class='qtybutton' style='display:none;'>-</button></div><div>£" + JSON.stringify(productlist.price) + "</div><div>£" + JSON.stringify(productlist.totalPrice) + "</div></div>")
 
                     });
                       $('#soldproducts').append("<br><div id='apiadditionalinfoheader'><h3>Additional Information</h3></div><br> <div id='apiaddinfo'><div><h4>Payment Details</h4>Type:" + JSON.stringify(string.paymentMethod) + "<br>Grand Total: £" + JSON.stringify(string.finalGrandTotal) + "<br>Status: " + JSON.stringify(string.paymentStatus) + "<br>Transaction ID: " + JSON.stringify(string.paymentTransactionId) + "</div><div><h4>Shipping Details</h4><br>Cost: £" + JSON.stringify(string.shippingFees) + "<br>Method: " + JSON.stringify(string.shippingMethod) + "<br>Tracking Number: <textarea id='trackingnumber' readonly>" + JSON.stringify(string.trackingNumber) + "</textarea>" );
@@ -98,8 +103,10 @@ $("#executeButton").click (function(e) {
                         $("#apieditbutton").click (function() {
                           $(this).unbind('click');
                           $('select').prop("disabled", false);
-                          $('textarea').prop("readOnly", false)
-                          $('textarea').css({"background-color" : "white"})
+                          $('textarea, input').prop("readOnly", false);
+                          $('textarea, input').css({"background-color" : "white"});
+                          $('#plus').css({"display" : "block"});
+                          $('#minus').css({"display" : "block"});
                           $('#orderhead').append("<button type='button' id='ammendorder' name='ammendorder'>Update</button>")
                         $('select').on('change', function() {
                             var statusselect = this.value
@@ -161,7 +168,7 @@ $("#executeButton").click (function(e) {
                                   email:$('#customeremail').val(),
                                 },
                                 customFields: [
-                                  {value:$('#customerphone').val()},
+                                  {value:$('#deliverynote').val()},
                                 ]
 
 
