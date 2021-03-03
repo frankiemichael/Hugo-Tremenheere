e7e4159fc8d35dabe863968782e70fcc541ed2d6
frankiemichael <frankiemichael96@gmail.com>
2021-03-03 16:45:05 +0000

    Add files via upload

--- surreal_succulents/static/js/jqueryproducts.js
+++ surreal_succulents/static/js/jqueryproducts.js
@@ -1,6 +1,7 @@
 $( document ).ready(function() {
 var host = "https://app.snipcart.com/api/products";
 var secret = "ST_ZTg1YjU2ZTgtMmVjZi00ZGZlLWE2NmItOTAzMjE3MjczMGRkNjM3NDk5MzMxNTMzMzYwMTEz"
+var id = "TREREIFE"
 $.ajax({
   type:"GET",
   url: host,
@@ -18,8 +19,10 @@ $.ajax({
     $.each(products, function(i,val){
       var listproducts = products[i]
       console.log(listproducts)
-      $('#apidata').append("<table><tr><td id='tdId'>" + listproducts.userDefinedId + "</td><td id='tdName'>" + listproducts.name + "<button id='expandOptions'>></button></td><td id='tdSku'>" + listproducts.customFields[0].value + "</td><td id='tdStock'>" + listproducts.stock + "</tr></table>")
+      $('#stockTable').append("<tr><td id='tdId'>" + listproducts.userDefinedId + "</td><td id='tdName'>" + listproducts.name + "<button id='expandOptions'>></button></td><td id='tdSku'>" + listproducts.customFields[0].value + "</td><td id='tdStock'>" + listproducts.stock + "</tr><br>")
     })
+    $('tr:not(:has(th)):not(:contains('TREREIFE'))").hide();
+    $('th').parent
 },
   error: function(error) {
   console.log(error)
