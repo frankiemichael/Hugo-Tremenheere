87e36ad06a92772bd144d88d00b28396d0d7e9fc
frankiemichael <frankiemichael96@gmail.com>
2021-03-04 16:56:56 +0000

    Add files via upload

--- surreal_succulents/static/js/jqueryproducts.js
+++ surreal_succulents/static/js/jqueryproducts.js
@@ -19,13 +19,44 @@ $.ajax({
     $.each(products, function(i,val){
       var listproducts = products[i]
       console.log(listproducts)
-      $('#stockTable').append("<tr><td id='tdId'>" + listproducts.userDefinedId + "</td><td id='tdName'>" + listproducts.name + "<button id='expandOptions'>></button></td><td id='tdSku'>" + listproducts.customFields[0].value + "</td><td id='tdStock'>" + listproducts.stock + "</tr><br>")
+      $('#stockTable').append("<tr><td id='tdId'><span>" + listproducts.userDefinedId + "</span></td><td id='tdName'><a style='text-decoration:none;color:black;'>" + listproducts.name + "</a><button id='expandOptions'>></button></td><td id='tdSku'><input type='text' id='changeSku"+ i + "' value='" + listproducts.customFields[0].value + "'></input></td><td id='tdStock'><input type='text' id='changeStock"+ i + "' value='" + listproducts.stock + "'></input></tr><br>")
     })
+      $('input').on('change', function(){
+        var idFind = $(this).parent().parent();
+        var id = $(this).parent().parent().find('span').text();
+        var skuUpdate = [{'value':$(this).val()},]
+
+        console.log(skuUpdate)
+        $.ajax({
+          type:"PUT",
+          url: host + "/" + id,
+          headers: {
+            'Authorization': `Basic ${btoa(secret)}`,
+            'Accept': 'application/json'
+          },
+          contentType: "application/json",
+          dataType: 'json',
+          data: JSON.stringify({ customFields: skuUpdate }),
+
+          success: function(result){
+            console.log(result)
+            console.log(this)
+          },
+          error: function(error) {
+          console.log(error)
+        }
+        })
+
+      })
+
+
     $('tr:not(:has(th)):not(:contains("TREREIFE"))').hide();
-    $('th').parent
+
+
 },
   error: function(error) {
   console.log(error)
 }
+
 });
 });
